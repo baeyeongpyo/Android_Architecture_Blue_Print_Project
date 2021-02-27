677efd2e103b92bf6f4d5410f2e68184bba5758b
Baecode <byp93@naver.com>
2021-02-27 20:03:35 +0900

    git init

---
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yeong.android_architecture_blue_print_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
