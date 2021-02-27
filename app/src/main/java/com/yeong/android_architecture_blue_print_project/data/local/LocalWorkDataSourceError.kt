de6962a48d636e13c428a9b4c66f698e3c6fdcb9
Baecode <byp93@naver.com>
2021-02-27 21:26:26 +0900

    local dataSource connection to room dao.

---
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/data/local/LocalWorkDataSourceError.kt
@@ -0,0 +1,8 @@
+package com.yeong.android_architecture_blue_print_project.data.local
+
+object LocalWorkDataSourceError {
+
+    const val DATA_EMPTY = 1
+    const val DATA_EMPTY_STRING = "empty work data"
+
+}
--- app/src/main/java/com/yeong/android_architecture_blue_print_project/data/local/LocalWorkDataSourceImpl.kt
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/data/local/LocalWorkDataSourceImpl.kt
@@ -3,22 +3,35 @@ package com.yeong.android_architecture_blue_print_project.data.local
 import com.yeong.android_architecture_blue_print_project.data.Result
 import com.yeong.android_architecture_blue_print_project.data.Work
 import com.yeong.android_architecture_blue_print_project.data.WorkDataSource
+import com.yeong.android_architecture_blue_print_project.data.local.LocalWorkDataSourceError.DATA_EMPTY
+import com.yeong.android_architecture_blue_print_project.data.local.LocalWorkDataSourceError.DATA_EMPTY_STRING
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class LocalWorkDataSourceImpl(
     private val dao: WorkDAO,
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : WorkDataSource {
-    override suspend fun getAllWork(): Result<List<Work>> {
-        TODO("Not yet implemented")
+    override suspend fun getAllWork(): Result<List<Work>> = withContext(dispatcher) {
+        try {
+            val result = dao.allWork()
+            if (!result.isNullOrEmpty()) {
+                return@withContext Result.Success(result)
+            } else {
+                return@withContext Result.Fail(DATA_EMPTY, Exception(DATA_EMPTY_STRING))
+            }
+        } catch (e: Exception) {
+            return@withContext Result.Fail(e)
+        }
     }
 
-    override suspend fun insertWork(vararg work: Work) {
-        TODO("Not yet implemented")
+    override suspend fun insertWork(vararg work: Work) = withContext(dispatcher) {
+        dao.addWork(*work)
     }
 
-    override suspend fun deleteWork(vararg work: Work) {
-        TODO("Not yet implemented")
+    override suspend fun deleteWork(vararg work: Work) = withContext(dispatcher){
+        dao.removeWork(*work)
     }
 }
