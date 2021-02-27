ce138bee5f415a0ca8675a4fc5e4464389cb5b75
Baecode <byp93@naver.com>
2021-02-27 21:24:58 +0900

    fix result generic type

--- app/src/main/java/com/yeong/android_architecture_blue_print_project/data/Result.kt
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/data/Result.kt
@@ -2,7 +2,10 @@ package com.yeong.android_architecture_blue_print_project.data
 
 import java.lang.Exception
 
-sealed class Result<T : Any> {
-    data class Success<T : Any>(val data: T) : Result<T>()
-    data class Fail(val code: Int, val exception: Exception) : Result<Nothing>()
+sealed class Result<out R : Any> {
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Fail(val code: Int, val exception: Exception) : Result<Nothing>(){
+        constructor(exception: Exception) : this(0, exception)
+    }
+
 }
