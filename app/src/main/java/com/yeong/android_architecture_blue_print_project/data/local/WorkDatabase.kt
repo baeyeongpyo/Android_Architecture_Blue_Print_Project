a9f06c70ea8c6d77d73f5a2f45179ef42e3db383
Baecode <byp93@naver.com>
2021-02-27 22:37:11 +0900

    WorkDatabase Extends RoomDataBase add.

--- app/src/main/java/com/yeong/android_architecture_blue_print_project/data/local/WorkDatabase.kt
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/data/local/WorkDatabase.kt
@@ -1,9 +1,10 @@
 package com.yeong.android_architecture_blue_print_project.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.yeong.android_architecture_blue_print_project.data.Work
 
 @Database(entities = [Work::class], version = 1)
-abstract class WorkDatabase {
+abstract class WorkDatabase : RoomDatabase(){
     abstract fun workDao(): WorkDAO
 }
