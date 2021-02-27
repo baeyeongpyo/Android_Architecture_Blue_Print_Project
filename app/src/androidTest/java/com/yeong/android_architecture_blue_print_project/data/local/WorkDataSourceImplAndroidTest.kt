a5f0f38ae550c0edab4e9a6eb862c2d8b5856a50
Baecode <byp93@naver.com>
2021-02-27 22:54:17 +0900

    remove test code add.

--- app/src/androidTest/java/com/yeong/android_architecture_blue_print_project/data/local/WorkDataSourceImplAndroidTest.kt
+++ app/src/androidTest/java/com/yeong/android_architecture_blue_print_project/data/local/WorkDataSourceImplAndroidTest.kt
@@ -6,6 +6,8 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.yeong.android_architecture_blue_print_project.data.Work
 import junit.framework.TestCase
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -32,7 +34,7 @@ class WorkDataSourceImplAndroidTest : TestCase() {
     }
 
     @Test
-    fun workDataBase_add_work_test() {
+    fun workDataBase_add_work_test() = runBlocking {
         val beforeWorkList = workDao.allWork()
 
         val newWorkData = Work(null, "test title", "test content data")
@@ -43,4 +45,20 @@ class WorkDataSourceImplAndroidTest : TestCase() {
         assertEquals(beforeWorkList.size + 1, afterWorkList.size)
     }
 
+    @Test
+    fun workDataBase_add_work_remove_test() = runBlocking {
+        val beforeWorkList = workDao.allWork()
+
+        val newWorkData = Work(null, "test title", "test content data")
+        workDao.addWork(newWorkData)
+
+        val removeBeforeList = workDao.allWork()
+
+        assertEquals(beforeWorkList.size + 1, removeBeforeList.size)
+
+        workDao.removeWork(removeBeforeList[0])
+        val removeAfterListData = workDao.allWork()
+
+        assertEquals(removeBeforeList.size - 1, removeAfterListData.size)
+    }
 }
