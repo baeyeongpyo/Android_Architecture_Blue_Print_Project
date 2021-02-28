bc16b3ac46d79386886cd2a687630092f1ac604b
Baecode <byp93@naver.com>
2021-02-28 23:57:03 +0900

    result Ext type cast exception error fix.

--- app/src/main/java/com/yeong/android_architecture_blue_print_project/util/ResultExt.kt
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/util/ResultExt.kt
@@ -1,19 +1,20 @@
 package com.yeong.android_architecture_blue_print_project.util
 
 import com.yeong.android_architecture_blue_print_project.data.Result
-import java.lang.Exception
 
 object ResultExt {
 
     fun <T : Any> Result<T>.onSuccess(bind: (T) -> Unit): Result<T> {
-        val result = this as Result.Success<T>
-        bind(result.data)
+        if (this is Result.Success<T>) {
+            bind(data)
+        }
         return this
     }
 
     fun <T : Any> Result<T>.onFail(bind: (Int, Exception) -> Unit): Result<T> {
-        val result = this as Result.Fail
-        bind(result.code, result.exception)
+        if (this is Result.Fail) {
+            bind(code, exception)
+        }
         return this
     }
 
