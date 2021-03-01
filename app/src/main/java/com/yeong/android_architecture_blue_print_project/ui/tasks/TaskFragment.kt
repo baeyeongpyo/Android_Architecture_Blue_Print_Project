b12ce6929ff52234fc6dd0956685fa70247a965a
Baecode <byp93@naver.com>
2021-03-01 13:09:45 +0900

    task fragment add.

---
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/ui/tasks/TaskFragment.kt
@@ -0,0 +1,19 @@
+package com.yeong.android_architecture_blue_print_project.ui.tasks
+
+import com.yeong.android_architecture_blue_print_project.BaseFragment
+import com.yeong.android_architecture_blue_print_project.R
+import com.yeong.android_architecture_blue_print_project.databinding.FragmentTaskBinding
+
+class TaskFragment : BaseFragment<FragmentTaskBinding>() {
+
+    override val layoutId: Int
+        get() = R.layout.fragment_task
+
+    override fun initView() {
+
+    }
+
+    override fun initBinding() {
+
+    }
+}
