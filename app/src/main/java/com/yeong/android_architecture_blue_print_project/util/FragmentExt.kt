3aa4d3cc70c797d1b0437db6715f930ce8d2234f
Baecode <byp93@naver.com>
2021-03-01 13:16:30 +0900

    fragment replace ext add.

---
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/util/FragmentExt.kt
@@ -0,0 +1,20 @@
+package com.yeong.android_architecture_blue_print_project.util
+
+import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
+object FragmentExt {
+
+    fun FragmentManager.replace(
+        @IdRes containerId: Int,
+        fragmentClass: Class<out Fragment>,
+        args: Bundle? = null
+    ) {
+        beginTransaction()
+            .replace(containerId, fragmentClass, args)
+            .commit()
+    }
+
+}
