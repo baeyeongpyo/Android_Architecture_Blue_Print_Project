da28195b77aa3370d0facf40d068c8647f81d83e
Baecode <byp93@naver.com>
2021-03-01 13:06:31 +0900

    base activity & base fragment abstract class add.

---
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/BaseActivity.kt
@@ -0,0 +1,24 @@
+package com.yeong.android_architecture_blue_print_project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
+
+    protected lateinit var viewBinding: T
+        private set
+
+    protected abstract val layoutId: Int
+
+    abstract fun initView()
+    abstract fun initBinding()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = DataBindingUtil.setContentView(this, layoutId)
+        initView()
+        initBinding()
+    }
+}
---
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/BaseFragment.kt
@@ -0,0 +1,36 @@
+package com.yeong.android_architecture_blue_print_project
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding> : Fragment() {
+
+    protected lateinit var viewBinding: T
+        private set
+
+    protected abstract val layoutId: Int
+
+    abstract fun initView()
+    abstract fun initBinding()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return viewBinding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+        initBinding()
+    }
+
+}
