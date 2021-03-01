9e9a24ddc449326c17a2681c9d0dfabfc3c47859
Baecode <byp93@naver.com>
2021-03-01 12:55:29 +0900

    ui package add & MainActivity path replace

--- app/src/main/java/com/yeong/android_architecture_blue_print_project/ui/main/MainActivity.kt
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yeong.android_architecture_blue_print_project
+package com.yeong.android_architecture_blue_print_project.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yeong.android_architecture_blue_print_project.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
