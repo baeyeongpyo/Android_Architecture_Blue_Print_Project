5010c5eebcf150a8f9341ec1e77640bdb6fefb04
Baecode <byp93@naver.com>
2021-03-01 13:22:27 +0900

    mainActivity add fragment

--- app/src/main/java/com/yeong/android_architecture_blue_print_project/ui/main/MainActivity.kt
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/ui/main/MainActivity.kt
@@ -3,6 +3,8 @@ package com.yeong.android_architecture_blue_print_project.ui.main
 import com.yeong.android_architecture_blue_print_project.BaseActivity
 import com.yeong.android_architecture_blue_print_project.R
 import com.yeong.android_architecture_blue_print_project.databinding.ActivityMainBinding
+import com.yeong.android_architecture_blue_print_project.ui.tasks.TaskFragment
+import com.yeong.android_architecture_blue_print_project.util.FragmentExt.replace
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
@@ -10,7 +12,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         get() = R.layout.activity_main
 
     override fun initView() {
-
+        supportFragmentManager.replace(R.id.fragment_container, TaskFragment::class.java)
     }
 
     override fun initBinding() {
