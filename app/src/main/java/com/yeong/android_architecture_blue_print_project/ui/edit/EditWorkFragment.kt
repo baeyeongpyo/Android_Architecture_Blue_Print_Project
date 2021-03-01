377318d2052665e7b94922c565239a52a2a642b8
Baecode <byp93@naver.com>
2021-03-01 16:32:16 +0900

    work edit fragment add.

---
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/ui/edit/EditWorkFragment.kt
@@ -0,0 +1,20 @@
+package com.yeong.android_architecture_blue_print_project.ui.edit
+
+import com.yeong.android_architecture_blue_print_project.BaseFragment
+import com.yeong.android_architecture_blue_print_project.R
+import com.yeong.android_architecture_blue_print_project.databinding.FragmentEditBinding
+
+class EditWorkFragment : BaseFragment<FragmentEditBinding>() {
+
+    override val layoutId: Int
+        get() = R.layout.fragment_edit
+
+    override fun initView() {
+
+    }
+
+    override fun initBinding() {
+
+    }
+
+}
