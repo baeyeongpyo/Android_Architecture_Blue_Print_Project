6fff0c2b55c87287562f8d914e5def52d88cfeaa
Baecode <byp93@naver.com>
2021-03-03 12:48:01 +0900

    create detail fragment

---
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/ui/detail/DetailWorkFragment.kt
@@ -0,0 +1,19 @@
+package com.yeong.android_architecture_blue_print_project.ui.detail
+
+import com.yeong.android_architecture_blue_print_project.BaseFragment
+import com.yeong.android_architecture_blue_print_project.R
+import com.yeong.android_architecture_blue_print_project.databinding.FragmentTaskDetailBinding
+
+class DetailWorkFragment : BaseFragment<FragmentTaskDetailBinding>() {
+
+    override val layoutId: Int
+        get() = R.layout.fragment_task_detail
+
+    override fun initView() {
+
+    }
+
+    override fun initBinding() {
+
+    }
+}
