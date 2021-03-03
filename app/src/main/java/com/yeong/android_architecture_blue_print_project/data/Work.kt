daf33050905c734dae87f7107a9bd7e0688f59ae
Baecode <byp93@naver.com>
2021-03-03 23:41:26 +0900

    work data parcelize add.

--- app/src/main/java/com/yeong/android_architecture_blue_print_project/data/Work.kt
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/data/Work.kt
@@ -1,9 +1,12 @@
 package com.yeong.android_architecture_blue_print_project.data
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 @Entity
 data class Work(
     @PrimaryKey(autoGenerate = true) val workIndex: Int?,
@@ -11,7 +14,7 @@ data class Work(
     @ColumnInfo(name = "content") var content: String,
     @ColumnInfo(name = "isComplete") var isComplete: Boolean = false, // added in version 2
     @ColumnInfo(name = "workCreateDate") var workCreateDate: Long = System.currentTimeMillis() // added in version 3
-) {
+) : Parcelable {
     constructor(title: String, content: String) : this(
         null,
         title,
