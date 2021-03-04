9ce59e2ab08662ee35bc84b3e4efde69b8e0dcac
Baecode <byp93@naver.com>
2021-03-04 14:29:34 +0900

    tasks viewModel create.

---
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/ui/ViewModelFactory.kt
@@ -0,0 +1,52 @@
+package com.yeong.android_architecture_blue_print_project.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.yeong.android_architecture_blue_print_project.data.WorkRepository
+import com.yeong.android_architecture_blue_print_project.data.WorkRepositoryImpl
+import com.yeong.android_architecture_blue_print_project.data.local.LocalWorkDataSourceImpl
+import com.yeong.android_architecture_blue_print_project.data.local.WorkDAO
+import com.yeong.android_architecture_blue_print_project.data.local.WorkDatabase
+import com.yeong.android_architecture_blue_print_project.ui.tasks.TaskViewModel
+
+class ViewModelFactory private constructor(private val application: Application) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    companion object {
+
+        private var INSTANCE: ViewModelFactory? = null
+
+        fun getInstance(application: Application): ViewModelFactory {
+            if (INSTANCE == null) {
+                INSTANCE = ViewModelFactory(application)
+            }
+            return INSTANCE!!
+        }
+
+    }
+
+    private val workDao: WorkDAO by lazy {
+        Room.databaseBuilder(application, WorkDatabase::class.java, "work.db")
+            .build()
+            .workDao()
+    }
+
+
+    private val workRepository: WorkRepository by lazy {
+        WorkRepositoryImpl(
+            LocalWorkDataSourceImpl(workDao)
+        )
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+        when {
+            modelClass.isAssignableFrom(TaskViewModel::class.java) -> {
+                TaskViewModel(workRepository) as T
+            }
+            else -> throw Exception("not found viewModel class : ${modelClass.canonicalName}")
+        }
+
+}
---
+++ app/src/main/java/com/yeong/android_architecture_blue_print_project/ui/tasks/TaskViewModel.kt
@@ -0,0 +1,7 @@
+package com.yeong.android_architecture_blue_print_project.ui.tasks
+
+import androidx.lifecycle.ViewModel
+import com.yeong.android_architecture_blue_print_project.data.WorkRepository
+
+class TaskViewModel(private val workRepo: WorkRepository) : ViewModel() {
+}
