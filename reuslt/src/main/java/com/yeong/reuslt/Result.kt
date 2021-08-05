bfedd69989dad1d5495bede964bb6c245975f464
Baecode <byp93@naver.com>
2021-08-05 23:11:42 +0900

    add result class code.

--- reuslt/src/main/java/com/yeong/reuslt/Result.kt
+++ reuslt/src/main/java/com/yeong/reuslt/Result.kt
@@ -1,4 +1,21 @@
 package com.yeong.reuslt
 
-class Result {
+import java.lang.Exception
+
+sealed class Result<out R> {
+
+    data class Success<T>(val data: T) : Result<T>()
+    data class Fail(val exception: Exception) : Result<Exception>()
+    object Loading : Result<Nothing>()
+    object Done : Result<Nothing>()
+
+    override fun toString(): String {
+        return when (this) {
+            is Success -> "Success[$data]"
+            is Fail -> "Fail[${exception.message}]"
+            is Loading -> "Loading"
+            is Done -> "Done"
+        }
+    }
+
 }
